ec71c0db7299aa479aada3d87e9d47610ea67585
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-11-05 15:39:34 -0700

    PDF solved

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import PDFViewer from 'pdf-viewer-reactjs';
+import {Document, Page} from 'react-pdf';
+import {rImage} from '../images/shortcut.png'
 class Resume extends Component {
   render() {
       return (
           <div>
-            <PDFViewer
-            document = {{url: 'http://www.pdf995.com/samples/pdf.pdf'}}
-            />
+           {/* <Document
+           file ="../images/ConnorResume.pdf"
+           onLoadSuccess={this.onDocumentLoadSuccess}
+        ></Document> */}
+         <iframe src="https://drive.google.com/file/d/1qwH-1G3NnFuCDNaVr6eLX5-olnGauvec/preview" width="100%" height="1000"></iframe>
+                      
           </div>
       )
   }
