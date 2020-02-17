f5dba15d96a6ce5ad1462bda12d25c843bf248df
MantisLanguage <connor.j.dougherty9@gmail.com>
2020-02-17 15:08:28 -0700

    updating resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ class Resume extends Component {
   render() {
       return (
           <div>
-         <iframe title="resume" src="https://drive.google.com/file/d/1t1Ho4Ewcr89uuDfjvf0ui_XU3BySoop0/preview" width="100%" height="1000"></iframe>         
+         <iframe title="resume" src="https://drive.google.com/file/d/1uWd6QCNEMyXuzKd74S_1s1G0zhc5alhp/preview" width="100%" height="1000"></iframe>         
           </div>
       )
   }
