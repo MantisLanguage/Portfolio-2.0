38fb3381b7f1074b8aa6c5f77bcb0322d94c2ec7
MantisLanguage <connor.j.dougherty9@gmail.com>
2020-02-17 15:14:50 -0700

    updating security threats and rebuilding for push

--- build/precache-manifest.1b2203fc4c9f195bafda5fca9b2efced.js
+++ build/precache-manifest.1b2203fc4c9f195bafda5fca9b2efced.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1fd36c0437abe3a2e19f891e174b864",
+    "revision": "ba180d82363dc166ded55d98c05e0e6d",
     "url": "/Portfolio-2.0/index.html"
   },
   {
-    "revision": "45c48eb0cc31cc62172b",
+    "revision": "9d146f51bdf45cb35a05",
     "url": "/Portfolio-2.0/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "25563bc6fde73233126d",
-    "url": "/Portfolio-2.0/static/css/main.7f40522d.chunk.css"
+    "revision": "edb46790d1f13b98ad19",
+    "url": "/Portfolio-2.0/static/css/main.d462ebd8.chunk.css"
   },
   {
-    "revision": "45c48eb0cc31cc62172b",
-    "url": "/Portfolio-2.0/static/js/2.2d25d54c.chunk.js"
+    "revision": "9d146f51bdf45cb35a05",
+    "url": "/Portfolio-2.0/static/js/2.90578c5b.chunk.js"
   },
   {
-    "revision": "25563bc6fde73233126d",
-    "url": "/Portfolio-2.0/static/js/main.ac9a658f.chunk.js"
+    "revision": "edb46790d1f13b98ad19",
+    "url": "/Portfolio-2.0/static/js/main.bd1a9c6c.chunk.js"
   },
   {
     "revision": "1077aab54f5e9d03b8ef",
@@ -26,10 +26,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "3f6fa2b5a9647ab4856ff3fe959109b1",
     "url": "/Portfolio-2.0/static/media/2018-07-12.3f6fa2b5.gif"
-  },
-  {
-    "revision": "bb786174be0788d1dbab2fa012992303",
-    "url": "/Portfolio-2.0/static/media/quizness.bb786174.jpg"
   },
   {
     "revision": "c1762cdc32358b2d1e914f9bcafca819",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-2.0/precache-manifest.209ef851251feac6d7c2aa153c8d1371.js"
+  "/Portfolio-2.0/precache-manifest.1b2203fc4c9f195bafda5fca9b2efced.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-2.0/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
