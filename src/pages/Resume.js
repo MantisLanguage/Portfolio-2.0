421c7a524c59a00fac1ec07209daba9d0977b1c1
Connor Dougherty <connor.j.dougherty9@gmail.com>
2020-02-26 22:35:34 -0700

    resume update

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ class Resume extends Component {
   render() {
       return (
           <div>
-         <iframe title="resume" src="https://drive.google.com/file/d/1t1Ho4Ewcr89uuDfjvf0ui_XU3BySoop0/preview" width="100%" height="1000"></iframe>         
+         <iframe title="resume" src="https://drive.google.com/file/d/1OlRI4b_Af5lbFFvuGNtSO7_Uy2KqnqyM/preview" width="100%" height="1000"></iframe>         
           </div>
       )
   }
